5288ca52a5472b5aaa4828b98865f3979529b633
David Harboe <davidharboe@gmail.com>
2013-01-07 17:00:26 +0100

    Git test . David Harboe hello world

---
+++ David Harboe/HelloWorld/HelloWorld/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by David Harboe on 1/7/13.
+//  Copyright (c) 2013 David Harboe. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
