47678c77b7cf6f560eee09ecb7c1a4c2a9ca8acb
Andreas Haure <andreas@haure.dk>
2013-01-09 14:57:42 +0100

    Testing again

--- s113023/HelloWorld/HelloWorld/main.m
+++ s113023/HelloWorld/HelloWorld/main.m
@@ -2,13 +2,11 @@
 //  main.m
 //  HelloWorld
 //
-//  Created by Andreas Haure on 08/01/13.
+//  Created by Andreas Haure on 09/01/13.
 //  Copyright (c) 2013 Andreas Roll Haure. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-
-//Test to see if local repo is working now after adding manualy
 
 int main(int argc, const char * argv[])
 {
