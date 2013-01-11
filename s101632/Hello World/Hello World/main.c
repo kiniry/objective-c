92710a615ec9b49d406fbc0a23fa0927c5230e0d
Jacob espersen <jacobespersen@gmail.com>
2013-01-11 17:44:57 +0100

    Hello world

---
+++ s101632/Hello World/Hello World/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Hello World
+//
+//  Created by Jacob Espersen on 11/01/13.
+//  Copyright (c) 2013 Jacob Espersen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
