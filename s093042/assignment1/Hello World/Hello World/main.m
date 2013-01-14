3e2ce99b9b05417dabd162b6b63dde279ccb14de
Sivanujann Selliah <sivanujan@gmail.com>
2013-01-14 14:33:24 +0100

    Moved Hello World into assignment1 folder and workspace

---
+++ s093042/assignment1/Hello World/Hello World/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by Sivanujann Selliah on 14/01/13.
+//  Copyright (c) 2013 Sivanujann Selliah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
