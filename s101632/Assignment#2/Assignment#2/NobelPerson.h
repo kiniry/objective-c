3a825fdccc83b796e30d4ee57ea12750375f5666
Jacob espersen <jacobespersen@gmail.com>
2013-01-14 23:39:41 +0100

    Start assignment 2

---
+++ s101632/Assignment#2/Assignment#2/NobelPerson.h
@@ -0,0 +1,13 @@
+//
+//  NobelPerson.h
+//  Assignment#2
+//
+//  Created by Jacob Espersen on 14/01/13.
+//  Copyright (c) 2013 Jacob Espersen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NobelPerson : NSObject
+
+@end
---
+++ s101632/Assignment#2/Assignment#2/NobelPerson.m
@@ -0,0 +1,13 @@
+//
+//  NobelPerson.m
+//  Assignment#2
+//
+//  Created by Jacob Espersen on 14/01/13.
+//  Copyright (c) 2013 Jacob Espersen. All rights reserved.
+//
+
+#import "NobelPerson.h"
+
+@implementation NobelPerson
+
+@end
