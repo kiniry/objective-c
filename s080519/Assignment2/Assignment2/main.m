c110b770b4874704dd4cb4fdda30c378a9d3cdc8
Mikkel Johnsen <mtjohnsen@gmail.com>
2013-01-14 12:19:22 +0100

    Starting on A2

---
+++ s080519/Assignment2/Assignment2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Assignment2
+//
+//  Created by Mikkel Johnsen on 14/01/13.
+//  Copyright (c) 2013 Mikkel Johnsen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
