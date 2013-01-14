56390c6be421eb043fdc50f6e22ae9551076613e
Markus Færevaag <mafaer@gmail.com>
2013-01-14 14:12:59 +0100

    Init Assignment2

---
+++ s123692/Assignment2/Assignment2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Assignment2
+//
+//  Created by Markus Færevaag on 14.01.13.
+//  Copyright (c) 2013 Markus Færevaag. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
