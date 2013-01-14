b43c7fb56b3283c862daafeccb68b446fe6de330
Søren Olofsson <soren@Srens-Mac.local>
2013-01-15 00:32:03 +0100

    Assignment 2 read me added

---
+++ s093030/assignment2/civilStatus/civilStatus/Experiment.h
@@ -0,0 +1,13 @@
+//
+//  ExperimentNil.h
+//  CivilStatus
+//
+//  Created by Søren Olofsson on 1/14/13.
+//  Copyright (c) 2013 Søren Olofsson. All rights reserved.
+//
+
+@interface Experiment : NSObject
+
+-(void) run;
+
+@end
