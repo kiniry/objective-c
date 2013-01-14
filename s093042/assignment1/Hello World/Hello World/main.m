43f72ae885cc55750bf1b289e3af5c20a3036d80
Sivanujann Selliah <sivanujan@gmail.com>
2013-01-14 14:53:50 +0100

    Reverted to older version

--- s093042/assignment1/Hello World/Hello World/main.m
+++ s093042/assignment1/Hello World/Hello World/main.m
@@ -2,7 +2,7 @@
 //  main.m
 //  Hello World
 //
-//  Created by Sivanujann Selliah on 14/01/13.
+//  Created by Sivanujann Selliah on 09/01/13.
 //  Copyright (c) 2013 Sivanujann Selliah. All rights reserved.
 //
 
