03da1399f60d1ec314fb7ad8f16d59704fa6efd8
Markus Færevaag <mafaer@gmail.com>
2013-01-14 14:40:19 +0100

    Init Citizen class

---
+++ s123692/Assignment2/Assignment2/Citizen.h
@@ -0,0 +1,17 @@
+//
+//  Citizen.h
+//  Assignment2
+//
+//  Created by Markus Færevaag on 14.01.13.
+//  Copyright (c) 2013 Markus Færevaag. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+@interface Citizen : NSObject
+
+
+
+@end
---
+++ s123692/Assignment2/Assignment2/Citizen.m
@@ -0,0 +1,15 @@
+//
+//  Citizen.m
+//  Assignment2
+//
+//  Created by Markus Færevaag on 14.01.13.
+//  Copyright (c) 2013 Markus Færevaag. All rights reserved.
+//
+
+#import "Citizen.h"
+
+@implementation Citizen
+
+
+
+@end
