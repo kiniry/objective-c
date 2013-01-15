c084c6db0027672b537a3177535645a1602fc19b
Jamie Neubert Pedersen <JamieNeubert@gmail.com>
2013-01-15 13:36:26 +0100

    Create NoblePerson class with some properties

---
+++ s113431/Assignment2/Assignment2/NoblePerson.h
@@ -0,0 +1,20 @@
+//
+//  NoblePerson.h
+//  Assignment2
+//
+//  Created by Jamie Neubert Pedersen on 1/13/13.
+//  Copyright (c) 2013 Jamie Neubert Pedersen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NoblePerson : NSObject
+
+@property (atomic) NSValue *fortune;
+@property (nonatomic) Citizen *spouse;
+
+- (void) setSpouse:(int *)spouse {
+    if()
+}
+
+@end
---
+++ s113431/Assignment2/Assignment2/NoblePerson.m
@@ -0,0 +1,16 @@
+//
+//  NoblePerson.m
+//  Assignment2
+//
+//  Created by Jamie Neubert Pedersen on 1/13/13.
+//  Copyright (c) 2013 Jamie Neubert Pedersen. All rights reserved.
+//
+
+#import "NoblePerson.h"
+#import "Citizen.h"
+
+@implementation NoblePerson
+
+@synthesize fortune = _fortune;
+
+@end
