800ba7bdca71baed7acc00c1cd4465db84643f31
Emil Klarskov Kristensen <emil.kristensen@jayway.com>
2013-01-16 23:08:25 +0100

    Initial Header file for Citizen

--- s083164/assignment2/Assignment2.1/Assignment2.1/Citizen.h
+++ s083164/assignment2/Assignment2.1/Assignment2.1/Citizen.h
@@ -8,6 +8,30 @@
 
 #import <Foundation/Foundation.h>
 
+typedef enum
+{
+    male,
+    female
+} Sex;
+
 @interface Citizen : NSObject
+
+// Name, Sex, Spouse, Children, Parents, Age, Single
+
+@property NSString *firstName;
+@property NSString *lastName;
+@property Sex sex;
+@property (strong) Citizen *spouse;
+@property (strong) Citizen *mother;
+@property (strong) Citizen *father;
+@property (strong) NSMutableSet *children;
+@property NSNumber *age;
+@property (readonly, getter=isSingle)BOOL single;
+
+-(Citizen *)initWithFirstName:(NSString *)firstName lastName:(NSString *)lastName sex:(Sex)sex andAge:(NSNumber *)age;
+
+-(BOOL)marry:(Citizen *)citizen;
+
+-(BOOL)divorce:(Citizen *)citizen;
 
 @end
