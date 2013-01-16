014a374c410879982a4936176a0e6aa53492782c
Emil Klarskov Kristensen <emil.kristensen@jayway.com>
2013-01-16 23:00:28 +0100

    Initial

---
+++ s083164/assignment2/Assignment2.1/Assignment2.1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Assignment2.1
+//
+//  Created by Emil Klarskov Kristensen on 1/16/13.
+//  Copyright (c) 2013 Emil Klarskov Kristensen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
