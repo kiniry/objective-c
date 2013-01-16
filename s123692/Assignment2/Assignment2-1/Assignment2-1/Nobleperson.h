0163859423630ae127980d2aef88b9e64b295c9d
Markus Færevaag <mafaer@gmail.com>
2013-01-16 11:47:58 +0100

    Refactored Assignment2

---
+++ s123692/Assignment2/Assignment2-1/Assignment2-1/Nobleperson.h
@@ -0,0 +1,25 @@
+//
+//  Nobleperson.h
+//  Assignment2
+//
+//  Created by Markus Færevaag on 14.01.13.
+//  Copyright (c) 2013 Markus Færevaag. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Citizen.h"
+
+@interface Nobleperson : Citizen
+{
+    NSNumber* _assets;
+    Citizen* _butler;
+}
+
+@property (nonatomic) NSNumber* assets;
+@property Citizen* butler;
+
+-(void) setAssets:(NSNumber *)assets;
+-(void) setAssetsWithoutSharing:(NSNumber *)assets;
+
+
+@end
