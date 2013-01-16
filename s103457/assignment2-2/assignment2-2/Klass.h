a54c7357d6dbb7f77b05f785bc568e94548da2ad
Anders Emil Nielsen <andersemil91@gmail.com>
2013-01-16 16:22:52 +0100

    First bunch of assignment2

---
+++ s103457/assignment2-2/assignment2-2/Klass.h
@@ -0,0 +1,13 @@
+//
+//  Klass.h
+//  assignment2-2
+//
+//  Created by Anders Emil Nielsen on 16/01/13.
+//  Copyright (c) 2013 Anders Emil Nielsen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Klass : NSObject
+
+@end
