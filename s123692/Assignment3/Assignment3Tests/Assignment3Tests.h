7110c53362fcb406d1dac42941765e8d6ca0c50c
Markus Færevaag <mafaer@gmail.com>
2013-01-17 14:06:05 +0100

    Init Assignment3

---
+++ s123692/Assignment3/Assignment3Tests/Assignment3Tests.h
@@ -0,0 +1,13 @@
+//
+//  Assignment3Tests.h
+//  Assignment3Tests
+//
+//  Created by Markus Færevaag on 17.01.13.
+//  Copyright (c) 2013 Markus Færevaag. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Assignment3Tests : SenTestCase
+
+@end
