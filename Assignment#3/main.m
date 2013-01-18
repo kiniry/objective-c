861de2f5ed6f444b031ac42d7e5d199a917cac80
Jacob espersen <jacobespersen@gmail.com>
2013-01-18 16:07:56 +0100

    Initial Commit

---
+++ Assignment#3/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Assignment#3
+//
+//  Created by Jacob Espersen on 18/01/13.
+//  Copyright (c) 2013 Jacob Espersen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
