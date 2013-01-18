88153017e596549a206ee1403257d2a63b17428e
Markus Færevaag <mafaer@gmail.com>
2013-01-18 03:21:24 +0100

    Implemented TableView, DetailView and removing

---
+++ s123692/Assignment3/Assignment3/Citizen.m
@@ -0,0 +1,131 @@
+//
+//  Citizen.m
+//  Assignment2
+//
+//  Created by Markus Færevaag on 14.01.13.
+//  Copyright (c) 2013 Markus Færevaag. All rights reserved.
+//
+
+#import "Citizen.h"
+
+@interface Citizen()
+
+// Private
+@property BOOL single;
+@property (readwrite, nonatomic) NSSet* children;
+@property (readwrite, nonatomic) NSSet* parents;
+
+@end
+
+
+@implementation Citizen
+
+@synthesize name = _name,
+sex = _sex,
+age = _age,
+single = _single,
+spouse = _spouse,
+children = _children,
+parents = _parents;
+
+// Constructor
+-(id) initWithName: (NSString *)name
+            andSex: (Sex)sex
+            andAge: (int)age
+{
+    self = [super init];
+    if (self)
+    {
+        _name = name;
+        _sex = sex;
+        _age = age;
+        _single = YES;
+        _children = [[NSSet alloc] init];
+        _parents = [[NSSet alloc] init];
+    }
+    return self;
+}
+
+// Parents
+-(void) addParent:(Citizen *)parent
+{
+    if ([self.parents count] > 2
+        || [self.parents containsObject:parent]
+        || [self.children containsObject:parent]
+        || (parent == self.spouse))
+    {
+        return;
+    }
+
+//    [parent addChild:self];
+    
+    NSMutableSet* tmp = [self.parents mutableCopy];
+    [tmp addObject:parent];
+    self.parents = [tmp copy];
+}
+-(void) removeParent:(Citizen *)parent
+{
+    if ([self.parents containsObject:parent])
+    {
+//        [parent removeChild:self];
+        
+        NSMutableSet* tmp = [self.parents mutableCopy];
+        [tmp removeObject:parent];
+        self.parents = [tmp copy];
+    }
+}
+
+// Children
+-(void) addChild:(Citizen *)child
+{
+    if ([self.parents containsObject:child]
+        || [self.children containsObject:child]
+        || (child == self.spouse))
+    {
+        return;
+    }
+    
+//    [child addParent:self];
+
+    NSMutableSet* tmp = [self.children mutableCopy];
+    [tmp addObject:child];
+    self.children = [tmp copy];
+}
+-(void) removeChild:(Citizen *)child
+{
+    if ([self.children containsObject:child])
+    {
+//        [child removeParent:self];
+        NSMutableSet* tmp = [self.children mutableCopy];
+        [tmp removeObject:child];
+        self.children = [tmp copy];
+    }
+}
+
+// Civil status
+-(void) marryTo: (Citizen *)fiancee
+{
+    if (self.single &&
+        !self.spouse &&
+        (fiancee.sex != self.sex) &&
+        ![self.children containsObject:fiancee] &&
+        ![self.parents containsObject:fiancee])
+    {
+        self.single = NO;
+        self.spouse = fiancee;
+        fiancee.single = NO;
+        fiancee.spouse = self;
+    }
+}
+
+-(void) divorceFrom: (Citizen *)spouse
+{
+    if (self.spouse)
+    {
+        self.single = YES;
+        self.spouse = nil;
+        [spouse divorceFrom:self];
+    }
+}
+
+@end
