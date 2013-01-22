3531bf6ed9219e86d109c2462fcb57212b71c612
Jamie Neubert Pedersen <JamieNeubert@gmail.com>
2013-01-22 18:51:53 +0100

    Initial commit Assignment 3

---
+++ s113431/Assignment3/Assignment3/Assertion.h
@@ -0,0 +1,13 @@
+//
+//  Assertion.h
+//  Assignment3
+//
+//  Created by Jamie Neubert Pedersen on 1/21/13.
+//  Copyright (c) 2013 Jamie Neubert Pedersen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Assertion : NSObject
+
+@end
