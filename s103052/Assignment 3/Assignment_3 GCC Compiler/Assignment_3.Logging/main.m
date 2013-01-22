0025803718b6cf29435bb73849267a44247fd116
Christian Poulsen <christian_poulsen@hotmail.com>
2013-01-22 20:41:25 +0100

    A3, Defensive + Assertion + Logging

--- s103052/Assignment 3/Assignment_3 GCC Compiler/Assignment_3.Logging/main.m
+++ s103052/Assignment 3/Assignment_3 GCC Compiler/Assignment_3.Logging/main.m
@@ -1,8 +1,8 @@
 /*************************************************************************
  *  File name   : main.m
- *  Description : Main module for Assignment 3 (Defensive) program.
+ *  Description : Main module for Assignment 3 (Logging) program.
  *
- *  Created on  : 17/01/2013
+ *  Created on  : 18/01/2013
  *  Author	  	: Christian Poulsen
  *
  *************************************************************************/
@@ -19,6 +19,8 @@ int main(int argc, const char * argv[])
 	* 1.0 - CONTAINS TESTS FOR THE SCENARIOS: MARRIAGE AND DIVORCE BETWEEN REGULAR CITIZENS.
 	*/
 	
+	NSLog(@"START TEST 1.0");
+	
 	// a) Create new regular Citizens
     Citizen* HansJensen = [[Citizen alloc] initWithName:@"Hans Jensen" withSex:@"Male" 
 											withAge:[NSNumber numberWithInt:45]];
@@ -34,43 +36,46 @@ int main(int argc, const char * argv[])
 											withAge:[NSNumber numberWithInt:32]];		
 	Citizen* EmilOlsen = [[Citizen alloc] initWithName:@"Emil Olsen" withSex:@"Male" 
 											withAge:[NSNumber numberWithInt:35]];	
-
+	
     /** Step 1) Add Children (OK) **/
-	[HansJensen addChild:AnnaNielsen];			// OK!
-	[MiaNielsen addChild:AnnaNielsen];			// OK!
+	NSLog(@"STEP 1)");
+	[HansJensen addChild:AnnaNielsen];		// OK!
+	[MiaNielsen addChild:AnnaNielsen];		// OK!
 	
 	/** Step 2) Add Children (FAIL) **/
-	//[AnnaNielsen addChild:HansJensen];		// Exception: "You are not allowed to add one of your own parents!"
-	//[AnnaNielsen addChild:MiaNielsen];		// Exception: "You are not allowed to add one of your own parents!"
-	//[HansJensen addChild:AnnaNielsen];		// Exception: "The citizen already has a father!"
-	//[MartinJensen addChild:AnnaNielsen];		// Exception: "The citizen already has a father!"
-	//[MiaNielsen addChild:AnnaNielsen];		// Exception: "The citizen already has a mother!"
-	//[MetteLarsen addChild:AnnaNielsen];		// Exception: "The citizen already has a mother!"
-	//[MetteLarsen addChild:nil];				// Exception: "You must specify a citizen!"
+	NSLog(@"STEP 2)");
+	[AnnaNielsen addChild:HansJensen];		// NSLOG: "You are not allowed to add one of your own parents!"
+	[MetteLarsen addChild:nil];				// NSLOG: "You must specify a citizen!"
+	[MartinJensen addChild:AnnaNielsen];	// NSLOG: "The citizen already has a father!"
+	[MetteLarsen addChild:AnnaNielsen];		// NSLOG: "The citizen already has a mother!"
 	
 	/** Step 3) Legal Marriage (OK) **/
-	[HansJensen marry:MiaNielsen];				// OK!
-	[AnnaNielsen marry:MartinJensen];			// OK!
+	NSLog(@"STEP 3)");
+	[HansJensen marry:MiaNielsen];			// OK!
+	[AnnaNielsen marry:MartinJensen];		// OK!
 	
 	/** Step 4) Not Legal Marriage (FAIL) **/
-	//[HansJensen marry:AnnaNielsen];			// Exception: "Not a legal marriage. Invalid spouse!" (Incest)
-	//[AnnaNielsen marry:HansJensen];			// Exception: "Not a legal marriage. Invalid spouse!" (Incest)
-	//[AnnaNielsen marry:AnnaNielsen];			// Exception: "Not a legal marriage. Invalid spouse!"
-	//[MetteLarsen marry:AnnaNielsen];			// Exception: "Not a legal marriage. Invalid spouse!" (Homosexuality)
-	//[EmilOlsen marry:SimonHansen];			// Exception: "Not a legal marriage. Invalid spouse!" (Homosexuality)
-	//[EmilOlsen marry:MiaNilsen];				// Exception: "Not a legal marriage. Invalid spouse!"
-	//[EmilOlsen marry:nil];					// Exception: "Not a legal marriage. Invalid spouse!"
+	NSLog(@"STEP 4)");
+	[HansJensen marry:AnnaNielsen];			// NSLOG: "Precondition violation: Not a legal marriage. Invalid spouse!" + "Precondition violation: Marriage failed!
+	[AnnaNielsen marry:HansJensen];			// NSLOG: "Precondition violation: Not a legal marriage. Invalid spouse!" + "Precondition violation: Marriage failed! 	
+	[AnnaNielsen marry:AnnaNielsen];		// NSLOG: "Precondition violation: Not a legal marriage. Invalid spouse!" + "Precondition violation: Marriage failed!
+	[MetteLarsen marry:AnnaNielsen];		// NSLOG: "Precondition violation: Not a legal marriage. Invalid spouse!" + "Precondition violation: Marriage failed!
+	[EmilOlsen marry:SimonHansen];			// NSLOG: "Precondition violation: Not a legal marriage. Invalid spouse!" + "Precondition violation: Marriage failed!
+	[EmilOlsen marry:MiaNielsen];			// NSLOG: "Precondition violation: Not a legal marriage. Invalid spouse!" + "Precondition violation: Marriage failed!
+	[EmilOlsen marry:nil];					// NSLOG: "Postcondition violation: You must specify a spouse!"
 	
 	/** Step 5) Legal Divorce (OK) **/
-    [MiaNielsen divorce:HansJensen];			// OK!
+	NSLog(@"STEP 5)");
+    [MiaNielsen divorce:HansJensen];		// OK!
         
 	/** Step 6) Not Legal Divorce (FAIL) **/
-	//[EmilOlsen divorce:MetteLarsen];			// Exception: "You are single and therefore unable to divorce this person!"
-    //[MiaNielsen divorce:HansJensen];			// Exception: "You are single and therefore unable to divorce this person!"
-	//[MartinJensen divorce:MetteLarsen];		// Exception: "The divorce is not possible!"
-	//[MartinJensen divorce:MartinJensen];		// Exception: "The divorce is not possible!"
-	//[MartinJensen divorce:nil];				// Exception: "The divorce is not possible!"
-	
+	NSLog(@"STEP 6)");
+	[MiaNielsen divorce:HansJensen];		// NSLOG: "Precondition violation: You are single and therefore unable to divorce this person!"
+	[EmilOlsen divorce:MetteLarsen];		// NSLOG: "Precondition violation: You are single and therefore unable to divorce this person!"
+	[MartinJensen divorce:nil];				// NSLOG: "Precondition violation: The divorce is not possible!"
+	[MartinJensen divorce:MartinJensen];	// NSLOG: "Precondition violation: The divorce is not possible!" + "Precondition violation: Divorce failed!" 
+	[MartinJensen divorce:MetteLarsen];		// NSLOG: "Precondition violation: The divorce is not possible!"
+
 	// Print Description of Test Citizens
 	NSLog(@"%@",[HansJensen description]);
 	NSLog(@"%@",[AnnaNielsen description]);
@@ -80,9 +85,13 @@ int main(int argc, const char * argv[])
 	NSLog(@"%@",[EmilOlsen description]);
 	NSLog(@"%@",[SimonHansen description]);
 	
+	NSLog(@"END TEST 1.0");
+	
 	/**
 	* 1.1 - CONTAINS TESTS FOR THE SCENARIOS: MARRIAGE BETWEEN NOBLE PERSONS
 	*/	
+	
+	NSLog(@"START TEST 1.1");
 	
     // a) Create new Noble Persons
     NoblePerson* AmandaAndersen = [[NoblePerson alloc] initWithName:@"Amanda Andersen" withSex:@"Female" 
@@ -100,39 +109,46 @@ int main(int argc, const char * argv[])
 										withSex:@"Male" withAge:[NSNumber numberWithInt:30]];
 	
 	/** Step 1) Add Children (OK) **/	
+	NSLog(@"STEP 1)");
 	[AmandaAndersen addChild:JanAndersen];			// OK!
 	[AmandaAndersen addChild:EmilOlsen]; 			// OK!
 	
 	/** Step 2) Add Butler (OK) **/
+	NSLog(@"STEP 2)");
 	[AmandaAndersen setButler:EmilOlsen];			// OK!
 	[AmandaAndersen setButler:Butler];				// OK!
 	[ChristineMadsen setButler:EmilOlsen];			// OK!
 	
-	/** Step 3) Add Butler (FAIL **/
-	//[JensKristensen setButler:ChristineMadsen];	// Exception: "You are not allowed to have a Noble Butler!"
-	//[JensKristensen setButler:JensKristensen];	// Exception: "You are not allowed to have a Noble Butler!"
-	//[AmandaAndersen setButler:nil];				// Exception: "You must specify a Citizen as Butler!"
+	/** Step 3) Add Butler (FAIL) **/
+	NSLog(@"STEP 3)");
+	[JensKristensen setButler:ChristineMadsen];		// NSLOG: "Precondition violation: You are not allowed to have a Noble Butler!"
+	[JensKristensen setButler:JensKristensen];		// NSLOG: "Precondition violation: You are not allowed to have a Noble Butler!"
+	[AmandaAndersen setButler:nil];					// NSLOG: "Precondition violation: You must specify a Citizen as Butler!"
 	
-	/** Step 4) Legal Marriage (OK) **/
+	/** Step 4) Legal Marriage (ASSERTION OK) **/
+	NSLog(@"STEP 4)");
 	[AmandaAndersen marryNoble:JensKristensen];		// OK!
         
-    /** Step 5) Not Legal Marriage (FAIL) **/
-	//[AmandaAndersen marryNoble:JanAndersen];   	// Exception: "Not a legal marriage. Invalid spouse!"
-	//[AmandaAndersen marryNoble:JensKristensen];	// Exception: "Not a legal marriage. Invalid spouse!"
-	//[ChristineMadsen marryNoble:AnnaJensen];		// Exception: "Not a legal marriage. Invalid spouse!"
-	//[AnnaJensen marryNoble:JanAndersen];			// Exception: "No marriage. You don't have a Butler!"
-	//[ChristineMadsen marryNoble:SimonHansen];		// Exception: "No marriage. You are not Noble!"
+    /** Step 5) Not Legal Marriage (ASSERTION FAIL) **/
+	NSLog(@"STEP 5)");
+	[AmandaAndersen marryNoble:JanAndersen];   		// NSLOG: "Not a legal marriage. Invalid spouse!"
+	[AmandaAndersen marryNoble:JensKristensen];		// NSLOG: "Not a legal marriage. Invalid spouse!"
+	[ChristineMadsen marryNoble:AnnaJensen];		// NSLOG: "Not a legal marriage. Invalid spouse!"
+	[AnnaJensen marryNoble:JanAndersen];			// NSLOG: "Precondition violation: No marriage. You don't have a Butler!"
+	[ChristineMadsen marryNoble:SimonHansen];		// NSLOG: "No marriage. You are not Noble!"
 	
-    /** Step 5) Legal Divorce (OK) **/
+    /** Step 6) Legal Divorce (ASSERTION OK) **/
+	NSLog(@"STEP 6)");
     [AmandaAndersen divorce:JensKristensen];		// OK!
         
-	/** Step 6) Not Legal Divorce (FAIL) **/
-	//[ChristineMadsen divorce:JanAndersen];		// Exception: "You are single and therefore unable to divorce this person!"
+	/** Step 7) Not Legal Divorce (ASSERTION FAIL) **/
+	NSLog(@"STEP 7)");
+	[ChristineMadsen divorce:JanAndersen];			// NSLOG: "Precondition violation: You are single and therefore unable to divorce this person!"
 
 	// Print Description of Test Noble Persons + Butler
 	NSLog(@"%@",[AmandaAndersen description]);
 	NSLog(@"%@",[JanAndersen description]);
-	NSLog(@"%@",[JensKristensen description]);
+	NSLog(@"%@",[JensKristensen description]);	
 	NSLog(@"%@",[ChristineMadsen description]);
 	NSLog(@"%@",[Butler description]);
 	
