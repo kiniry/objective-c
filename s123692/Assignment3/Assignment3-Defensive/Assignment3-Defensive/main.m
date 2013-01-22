1131bc84ce7a0b4a2aeb0ab4d3830f9373a1b27e
Markus Færevaag <mafaer@gmail.com>
2013-01-22 13:13:02 +0100

    Init Assignment3-Defensive

---
+++ s123692/Assignment3/Assignment3-Defensive/Assignment3-Defensive/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Assignment3-Defensive
+//
+//  Created by Markus Færevaag on 22.01.13.
+//  Copyright (c) 2013 Markus Færevaag. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
