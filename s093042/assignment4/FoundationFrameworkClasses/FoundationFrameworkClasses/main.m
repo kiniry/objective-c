4f1a1eeb737121404f557b252a7f913682dce469
Sivanujann Selliah <sivanujan@gmail.com>
2013-01-23 23:45:22 +0100

    more foundation framework tests

--- s093042/assignment4/FoundationFrameworkClasses/FoundationFrameworkClasses/main.m
+++ s093042/assignment4/FoundationFrameworkClasses/FoundationFrameworkClasses/main.m
@@ -12,9 +12,14 @@ int main(int argc, const char * argv[])
 {
     @autoreleasepool {
         NSUInteger numberOfIterations = 1000000;
+        NSUInteger numberOfElements = 100000;
         
         if(argc > 1)
+        {
             numberOfIterations = atoi(argv[1]);
+            if(argc > 2)
+                numberOfElements = atoi(argv[2]);
+        }
         
         // start the timer
         NSDate *start = [NSDate date];
@@ -66,21 +71,63 @@ int main(int argc, const char * argv[])
         NSLog(@"%lu iterations of calling the double number factory of NSNumber took %f ms", numberOfIterations, (timeInterval*-1000));
         
         
-        // NSArray count
-        NSUInteger numberOfElements = 100000;
-        NSMutableArray *tmpArray = [NSMutableArray arrayWithCapacity:100000];
+        // NSMutableArray count
+        NSMutableArray *tmpMArray = [NSMutableArray arrayWithCapacity:100000];
         for(NSUInteger i = 0; i < numberOfElements; i++)
-            [tmpArray addObject:[NSNumber numberWithInteger:i]];
+            [tmpMArray addObject:[NSNumber numberWithInteger:i]];
         // start the timer
         start = [NSDate date];
-        for(int i = 0; i < numberOfIterations; i++)
+        for(NSUInteger i = 0; i < numberOfIterations; i++)
+        {
+            [tmpMArray count];
+        }
+        timeInterval = [start timeIntervalSinceNow];
+        
+        // get the result
+        NSLog(@"%lu iterations of counting the elements of a NSMutableArray with %lu elements took %f ms", numberOfIterations, numberOfElements, (timeInterval*-1000));
+        
+        
+        // NSArray count
+        NSArray *tmpArray = [tmpMArray copy];
+        // start the timer
+        start = [NSDate date];
+        for(NSUInteger i = 0; i < numberOfIterations; i++)
         {
             [tmpArray count];
         }
         timeInterval = [start timeIntervalSinceNow];
         
         // get the result
-        NSLog(@"%lu iterations of counting the elements of a NSMutableArray took %f ms", numberOfIterations, (timeInterval*-1000));
+        NSLog(@"%lu iterations of counting the elements of a NSArray with %lu elements took %f ms", numberOfIterations, numberOfElements, (timeInterval*-1000));
+        
+        
+        // NSMutableDictionary get object
+        NSMutableDictionary *tmpMDictionary = [NSMutableDictionary dictionary];
+        for(NSUInteger i = 0; i < numberOfElements; i++)
+            [tmpMDictionary setObject:[NSNumber numberWithInteger:i] forKey:[NSString stringWithFormat:@"%lu", i]];
+        // start the timer
+        start = [NSDate date];
+        for(NSUInteger i = 0; i < numberOfIterations; i++)
+        {
+            [tmpMDictionary objectForKey:[NSString stringWithFormat:@"%lu", i]];
+        }
+        timeInterval = [start timeIntervalSinceNow];
+        
+        // get the result
+        NSLog(@"%lu iterations of getting an object for a key in a NSMutableDictionary with %lu elements took %f ms", numberOfIterations, numberOfElements, (timeInterval*-1000));
+        
+        // NSDictionary get object
+        NSDictionary *tmpDictionary = [tmpMDictionary copy];
+        // start the timer
+        start = [NSDate date];
+        for(NSUInteger i = 0; i < numberOfIterations; i++)
+        {
+            [tmpDictionary objectForKey:[NSString stringWithFormat:@"%lu", i]];
+        }
+        timeInterval = [start timeIntervalSinceNow];
+        
+        // get the result
+        NSLog(@"%lu iterations of getting an object for a key in a NSDictionary with %lu elements took %f ms", numberOfIterations, numberOfElements, (timeInterval*-1000));
         
         
     }
