bd32d8d1766e8a3fc6e138c477fd95eb38524e52
Markus Færevaag <mafaer@gmail.com>
2013-01-24 12:46:52 +0100

    Init Assignment4

---
+++ s123692/Assignment4/Assignment4/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Assignment4
+//
+//  Created by Markus Færevaag on 24.01.13.
+//  Copyright (c) 2013 Markus Færevaag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ s123692/Assignment4/Assignment4/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Assignment4
+//
+//  Created by Markus Færevaag on 24.01.13.
+//  Copyright (c) 2013 Markus Færevaag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
