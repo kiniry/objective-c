c0668c7101cfb2cf08e04f94b3edfd2807a2d872
Vlad Luca <lucavlad88@gmail.com>
2013-01-24 04:05:21 -0800

    assignment 1

---
+++ s111026/HelloWorld/HelloWorld/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Home User on 1/24/13.
+//  Copyright (c) 2013 Home User. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
