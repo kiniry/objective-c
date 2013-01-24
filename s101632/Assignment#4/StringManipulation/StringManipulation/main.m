8f0eda86fa7df23cf57b4b7a82a2209045197344
Jacob espersen <jacobespersen@gmail.com>
2013-01-24 16:39:17 +0100

    starting A4

--- s101632/Assignment#4/StringManipulation/StringManipulation/main.m
+++ s101632/Assignment#4/StringManipulation/StringManipulation/main.m
@@ -1,8 +1,8 @@
 //
 //  main.m
-//  Assignment#4
+//  StringManipulation
 //
-//  Created by Jacob Espersen on 23/01/13.
+//  Created by Jacob Espersen on 24/01/13.
 //  Copyright (c) 2013 Jacob Espersen. All rights reserved.
 //
 
@@ -13,8 +13,7 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSMutableString* str = [[NSMutableString alloc] init];
         
     }
     return 0;
