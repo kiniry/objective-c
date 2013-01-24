8a143d3525b3158c5016f92085dd0fa6a42f7088
Jacob espersen <jacobespersen@gmail.com>
2013-01-24 15:55:36 +0100

    start A4

---
+++ s101632/Assignment#4/Assignment#4/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Assignment#4
+//
+//  Created by Jacob Espersen on 23/01/13.
+//  Copyright (c) 2013 Jacob Espersen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
