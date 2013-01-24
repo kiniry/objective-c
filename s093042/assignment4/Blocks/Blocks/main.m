cd72392b5d0ec57d068302afa9c71f6355c9dede
Sivanujann Selliah <sivanujan@gmail.com>
2013-01-24 22:50:48 +0100

    Edited number of iterations and elements to run on default

--- s093042/assignment4/Blocks/Blocks/main.m
+++ s093042/assignment4/Blocks/Blocks/main.m
@@ -13,7 +13,7 @@
 int main(int argc, const char * argv[])
 {
     @autoreleasepool {
-        NSUInteger numberOfIterations = 1000;
+        NSUInteger numberOfIterations = 100000;
         
         if(argc > 1)
         {
