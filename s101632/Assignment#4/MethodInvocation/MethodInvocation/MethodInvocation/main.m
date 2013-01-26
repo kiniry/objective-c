e4fd8c9f40a660d1cc1e0ae212bb1750ff8ca0a0
Jacob espersen <jacobespersen@gmail.com>
2013-01-26 14:06:27 +0100

    A4 method invocation

--- s101632/Assignment#4/MethodInvocation/MethodInvocation/MethodInvocation/main.m
+++ s101632/Assignment#4/MethodInvocation/MethodInvocation/MethodInvocation/main.m
@@ -26,23 +26,39 @@ int main(int argc, const char * argv[])
             for (int j = 0; j<= iterations; j++) {
                 [methObj instanceDoNothing];
             }
-            NSLog(@"The time consumption for calling a instance method %d times is: %f ms", iterations, ([timestamp timeIntervalSinceNow]*-1000));
+            NSLog(@"The time consumption for calling a instance method %d times is: %0.2f ms", iterations, ([timestamp timeIntervalSinceNow]*-1000));
             
             //Calls the class method as many times as the value of "iterations" and log the time consumption
             timestamp = [NSDate date];
             for (int j = 0; j<= iterations; j++) {
                 [Methods classDoNothing];
             }
-            NSLog(@"The time consumption for calling a class method %d times is: %f ms", iterations, ([timestamp timeIntervalSinceNow]*-1000));
+            NSLog(@"The time consumption for calling a class method %d times is: %0.2f ms", iterations, ([timestamp timeIntervalSinceNow]*-1000));
+            
+            //invokes a selector of the instance method "doNothing" and logs the time
+            timestamp = [NSDate date];
+            SEL insSel = @selector(instanceDoNothing);
+            for (int j = 0; j<= iterations; j++) {
+                [methObj performSelector:insSel];
+            }
+            NSLog(@"The time consumption for invoking a selector of a instance method %d times is: %0.2f ms", iterations, ([timestamp timeIntervalSinceNow]*-1000));
+            
+            //invokes a selector of the class method "doNothing" and logs the time
+            timestamp = [NSDate date];
+            SEL classSel = @selector(classDoNothing);
+            for (int j = 0; j<= iterations; j++) {
+                [Methods performSelector:classSel];
+            }
+            NSLog(@"The time consumption for invoking a selector of a class method %d times is: %0.2f ms", iterations, ([timestamp timeIntervalSinceNow]*-1000));
             
             //Calls the block as many times as the value of "iterations" and log the time consumption
             timestamp =[NSDate date];
             for (int j = 0; j<= iterations; j++) {
                 doNothing();
             }
-            NSLog(@"The time consumption for calling a block %d times is: %f ms", iterations, ([timestamp timeIntervalSinceNow]*-1000));
-            iterations *= 2;
+            NSLog(@"The time consumption for calling a block %d times is: %0.2f ms", iterations, ([timestamp timeIntervalSinceNow]*-1000));
             
+            iterations *= 2;
         }
         
     }
