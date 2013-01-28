058fbb878c15e2a44541ee02f4fa2d78ed22999c
Markus Færevaag <mafaer@gmail.com>
2013-01-28 23:49:41 +0100

    Feedback on s030728's Assignment 2

---
+++ s123692/Assignment4/Assignment4/Blocks.h
@@ -0,0 +1,13 @@
+//
+//  Blocks.h
+//  Assignment4
+//
+//  Created by Markus Færevaag on 28.01.13.
+//  Copyright (c) 2013 Markus Færevaag. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Blocks : NSObject
+
+@end
---
+++ s123692/Assignment4/Assignment4/Blocks.m
@@ -0,0 +1,13 @@
+//
+//  Blocks.m
+//  Assignment4
+//
+//  Created by Markus Færevaag on 28.01.13.
+//  Copyright (c) 2013 Markus Færevaag. All rights reserved.
+//
+
+#import "Blocks.h"
+
+@implementation Blocks
+
+@end
