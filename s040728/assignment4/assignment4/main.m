6d7e86747ffe6eb91efccbfef8ff556f95c74fb9
magloire <magloire1@gmail.com>
2013-01-29 12:36:14 +0100

    profiling string + recursion

--- s040728/assignment4/assignment4/main.m
+++ s040728/assignment4/assignment4/main.m
@@ -15,11 +15,25 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         // insert code here...
-        int cost = [LevenshteinDistance distanceBetween:@"kitten" and:@"sitting"];
         
-        long cost1 = [LevenshteinDistance distanceUsingRecursionBetween:@"rosettacode" and:@"raisethysword"];
-        NSLog(@"the distance using iteration is %i", cost);
-        NSLog(@"the distance using recursion is %li", cost1);
+        NSLog(@"Before calling distanceBetween:");
+        NSDate *date1 = [NSDate date];
+        [LevenshteinDistance distanceBetween:@"distanceBetween" and:@"distanceUsingRecursion"];
+        
+        double elapsedTime1 = fabs([date1 timeIntervalSinceNow]);
+       
+        
+        NSLog(@"Before calling distanceUsingRecursion:");
+        NSDate *date2 = [NSDate date];
+        [LevenshteinDistance distanceUsingRecursionBetween:@"distanceBetween" and:@"distanceUsingRecursion"];
+        
+        double elapsedTime2 = fabs([date2 timeIntervalSinceNow]);
+        
+        
+        NSLog(@"the running time for distanceBetween: is %0.4f ms", elapsedTime1);
+        NSLog(@"the running time for distanceUsingRecursion is %0.4f ms", elapsedTime2);
+        
+        
         
     }
     return 0;
