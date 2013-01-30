32dbf9f67268b7f2f90d7d58a181adbf83ccb97c
Christian Poulsen <christian_poulsen@hotmail.com>
2013-01-30 17:52:55 +0100

    A2 Class files Done!

---
+++ s103052/Assignment 2/Assignment_2.2/Child.m
@@ -0,0 +1,19 @@
+/*************************************************************************
+ *  File name   : Child.h
+ *  Description : Test Class Child headerfile
+ *
+ *  Created on  : 21/01/2013
+ *  Author	  	: Christian Poulsen
+ *
+ *************************************************************************/
+
+#import "Child.h"
+
+@implementation Child
+
+- (NSString*)helloWorld
+{
+    return @"Hello World!";
+}
+
+@end
---
+++ s103052/Assignment 2/Assignment_2.2/ParentProtocol.h
@@ -0,0 +1,17 @@
+/*************************************************************************
+ *  File name   : ParentProtocol.h
+ *  Description : Test Class Parent Protocol
+ *
+ *  Created on  : 21/01/2013
+ *  Author	  	: Christian Poulsen
+ *
+ *************************************************************************/
+
+#import <Foundation/Foundation.h>
+
+@protocol ParentProtocol <NSObject>
+
+@required
+-(void)helloWorld;
+
+@end
