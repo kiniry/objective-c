e3d539b779d04a88c6999733f71ab8f6a5b66b32
Jacob Gjerstrup <jacobgjerstrup@hotmail.com>
2013-01-30 23:54:06 +0100

    Comitted assignment 3

---
+++ s113440/Assignment_2/Assignment2/BONCeo.m
@@ -0,0 +1,21 @@
+//
+//  BONCeo.m
+//  BONCITIZEN
+//
+//  Created by Bastian Buch on 1/22/13.
+//  Copyright (c) 2013 Bastian Buch, s113432, and Jacob Gjerstrup, s113440. All rights reserved.
+//
+
+#import "BONCeo.h"
+
+@implementation BONCeo
+
+// Implementing the actual protocols functions.
+- (id) work{
+    return NO;
+}
+
+- (bool) workIsDone {
+    return NO;
+}
+@end
