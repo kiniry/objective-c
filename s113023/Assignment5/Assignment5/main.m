5258d5e6a6bd0cddb1d5b8c1a062beccd6c58954
Andreas Haure <andreas@haure.dk>
2013-01-31 23:29:59 +0100

    Assignment 5 closer to completion

--- s113023/Assignment5/Assignment5/main.m
+++ s113023/Assignment5/Assignment5/main.m
@@ -14,7 +14,10 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        Random *randomNumber = [[Random alloc]initWithSeed:4000];
+        // Create instance of Random object
+        Random *randomNumber = [[Random alloc]init];
+        
+        printf("---/ Random integers /---\n");
         int firstRandom = [randomNumber nextInt];
         printf("%d\n",firstRandom);
         int secondRandom = [randomNumber nextInt];
@@ -23,48 +26,77 @@ int main(int argc, const char * argv[])
         printf("%d\n",thirdRandom);
         int fourthRandom = [randomNumber nextInt];
         printf("%d\n",fourthRandom);
+        printf("-------------------------\n\n");
         
-        int fifthRandom = [randomNumber nextInt:20];
-        printf("%d\n",fifthRandom);
-        int sixthRandom = [randomNumber nextInt:30];
-        printf("%d\n",sixthRandom);
-        int seventhRandom = [randomNumber nextInt:10];
-        printf("%d\n",seventhRandom);
+        printf("---/ Random integers with specified max /---\n");
+        int max1 = 5;
+        int fifthRandom = [randomNumber nextInt:max1];
+        printf("%d (max: %d)\n",fifthRandom,max1);
+        int max2 = 4;
+        int sixthRandom = [randomNumber nextInt:max2];
+        printf("%d (max: %d)\n",sixthRandom,max2);
+        int max3 = 3;
+        int seventhRandom = [randomNumber nextInt:max3];
+        printf("%d (max: %d)\n",seventhRandom,max3);
+        printf("--------------------------------------------\n\n");
         
+        printf("---/ Random longs /---\n");
         long eightRandom = [randomNumber nextLong];
         printf("%ld\n",eightRandom);
         long ninthRandom = [randomNumber nextLong];
         printf("%ld\n",ninthRandom);
         long tenthRandom = [randomNumber nextLong];
         printf("%ld\n",tenthRandom);
+        printf("----------------------\n\n");
         
+        printf("---/ Random booleans /---\n");
         BOOL eleventhRandom = [randomNumber nextBoolean];
         printf("%d\n",eleventhRandom);
         BOOL twelfthRandom = [randomNumber nextBoolean];
         printf("%d\n",twelfthRandom);
         BOOL thirteenthRandom = [randomNumber nextBoolean];
         printf("%d\n",thirteenthRandom);
+        printf("--------------------------\n\n");
         
+        printf("---/ Random floats /---\n");
         float fourteenth = [randomNumber nextFloat];
         printf("%f\n",fourteenth);
         float fifteenth = [randomNumber nextFloat];
         printf("%f\n",fifteenth);
         float sixteenth = [randomNumber nextFloat];
         printf("%f\n",sixteenth);
+        printf("-----------------------\n\n");
         
+        printf("---/ Random doubles /---\n");
         double seventeenth = [randomNumber nextDouble];
         printf("%f\n",seventeenth);
         double eigtheen = [randomNumber nextDouble];
         printf("%f\n",eigtheen);
         double nineteenth = [randomNumber nextDouble];
         printf("%f\n",nineteenth);
+        printf("-------------------------\n\n");
         
+        printf("---/ Random gaussian doubles /---\n");
         double twenty = [randomNumber nextGaussian];
         printf("%f\n",twenty);
         double twentyone = [randomNumber nextGaussian];
         printf("%f\n",twentyone);
         double twentytwo = [randomNumber nextGaussian];
         printf("%f\n",twentytwo);
+        printf("---------------------------------\n\n");
+        
+        /*
+        Random *randomNumber2 = [[Random alloc]init];
+        int firstRandom2 = [randomNumber2 nextInt];
+        printf("%d\n",firstRandom2);
+        int secondRandom2 = [randomNumber2 nextInt];
+        printf("%d\n",secondRandom2);
+        int thirdRandom2 = [randomNumber2 nextInt];
+        printf("%d\n",thirdRandom2);
+        int fourthRandom2 = [randomNumber2 nextInt];
+        printf("%d\n",fourthRandom2);
+        */
+    
              
     }
     return 0;
