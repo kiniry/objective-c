0f4f97dffe11a5532a9485aa144ac22c28b78462
Emil Klarskov Kristensen <emil.kristensen@jayway.com>
2013-02-01 17:45:44 +0100

    Added nodes and authentication

---
+++ s083164/assignment5/FacebookFun/FacebookFun/Auth.m
@@ -0,0 +1,13 @@
+//
+//  Auth.m
+//  FacebookFun
+//
+//  Created by Emil Klarskov Kristensen on 2/1/13.
+//  Copyright (c) 2013 Emil Klarskov Kristensen. All rights reserved.
+//
+
+#import "Auth.h"
+
+@implementation Auth
+
+@end
---
+++ s083164/assignment5/FacebookFun/FacebookFun/Node.m
@@ -0,0 +1,13 @@
+//
+//  Node.m
+//  FacebookFun
+//
+//  Created by Emil Klarskov Kristensen on 2/1/13.
+//  Copyright (c) 2013 Emil Klarskov Kristensen. All rights reserved.
+//
+
+#import "Node.h"
+
+@implementation Node
+
+@end
