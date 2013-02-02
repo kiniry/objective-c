0d957e4090118ff2c6eef1c3f475eadf35aa7a1b
Sivanujann Selliah <sivanujan@gmail.com>
2013-02-02 17:14:15 +0100

    More files added to assignment 5

--- s093042/assignment5/Process/Process/main.m
+++ s093042/assignment5/Process/Process/main.m
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "ProcessBuilder.h"
 
 int main(int argc, const char * argv[])
 {
@@ -15,7 +16,8 @@ int main(int argc, const char * argv[])
         
         // insert code here...
         NSLog(@"Hello, World!");
-        
+        ProcessBuilder *t = [ProcessBuilder new];
+        [t hejsa:@"test1", @"test2", @"test3", @"test4", nil];
     }
     return 0;
 }
