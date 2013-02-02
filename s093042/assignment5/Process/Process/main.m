4453fbaef167c962de01d7b5f7bbd155bc3d6aa2
Sivanujann Selliah <sivanujan@gmail.com>
2013-02-02 20:45:07 +0100

    Removed old files

--- s093042/assignment5/Process/Process/main.m
+++ s093042/assignment5/Process/Process/main.m
@@ -7,17 +7,12 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "ProcessBuilder.h"
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
-        ProcessBuilder *t = [ProcessBuilder new];
-        [t hejsa:@"test1", @"test2", @"test3", @"test4", nil];
     }
     return 0;
 }
