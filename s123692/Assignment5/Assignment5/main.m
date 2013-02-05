8cac3767edae2be31d49911651ee8f1c9c8fea99
Markus Færevaag <mafaer@gmail.com>
2013-02-05 10:42:30 +0100

    Init commit Assignment5

---
+++ s123692/Assignment5/Assignment5/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Assignment5
+//
+//  Created by Markus Færevaag on 05.02.13.
+//  Copyright (c) 2013 Markus Færevaag. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
